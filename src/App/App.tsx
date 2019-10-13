e9df3e5d79e6c9bd4bb59889d56c020df2ed39b7
Jack O'Mahony <jack@JKs-MacBook-Pro.local>
2019-10-13 23:21:26 +0100

    date formatting

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.scss';
 import { Store, IContraction } from '../Store'; 
+import stopwatch from './stopwatch.svg';
 
 const App: React.FC = () => {
   const {state, dispatch} = React.useContext(Store);
@@ -12,6 +13,9 @@ const App: React.FC = () => {
     }
   }
 
+  let recentDiff = state.contractions.length > 1 ?  dateDiffInSeconds(state.contractions[0].start, state.contractions[1].end!) : 0;
+
+
   return (
     <div className="App">
       <main>
@@ -22,10 +26,10 @@ const App: React.FC = () => {
           <button className="contraction" onClick={() => {dispatch({type: 'CONTRACTION_START', payload: new Date()}); dispatch({type: 'TOGGLE_CONTRACTION'})} }>start</button>
         }
         <div className="summary">
-          {state.contractions.length > 0 ? <TimeSinceLastContraction latestContraction={state.contractions[0]}></TimeSinceLastContraction> : null}
-          { state.contractions.length > 1 ? <Summary contractions={state.contractions.slice(0, 3)}></Summary> : null}
+          {state.contractions.length > 0 ? <TimeSinceLastContraction latestContraction={state.contractions[0]} recentDiff={recentDiff}></TimeSinceLastContraction> : null}
+          { state.contractions.length > 1 ? <Summary contractions={state.contractions.slice(0, 3)} recentDiff={recentDiff}></Summary> : null}
         </div>
-        
+
       <table>
         <thead>
           <tr>
@@ -77,35 +81,39 @@ const ContractionListItem: React.FunctionComponent<{contraction: IContraction}> 
   return (
     <tr>
       <td>
-        {contraction.start.toLocaleTimeString()}
+        <Time date={contraction.start}></Time>
       </td>
       {
-        contraction.end !== undefined ? <td>{contraction.end.toLocaleTimeString()}</td> : <td></td>
+        contraction.end !== undefined ? <td><Time date={contraction.end}></Time></td> : <td></td>
       }
       <td>
-          { formatTime(duration) }
+        <span className="timer">
+          {
+            contraction.end === undefined ? <img className="stopwatch-icon" src={stopwatch} /> : null
+          }
+            { formatDuration(duration) }
+        </span>
+        
       </td> 
     </tr>)
 }
 
-const Summary: React.FunctionComponent<{contractions: IContraction[]}> = ({contractions}) => {
+const Summary: React.FunctionComponent<{contractions: IContraction[]; recentDiff: number}> = ({contractions, recentDiff}) => {
 
   const latestContraction = contractions[0];
 
-  let recentDiff = dateDiffInSeconds(latestContraction.start, contractions[1].end!);
-
-  const recentDiffDisplay = <div className="recent-diff"><label>Time between last 2 contractions</label> {formatTime(recentDiff)}</div>
+  const recentDiffDisplay = <div className="recent-diff"><label>Time between last 2 contractions</label> {formatDuration(recentDiff)}</div>
       
   if(contractions.length === 3){
     let startDate = latestContraction.end === undefined ? latestContraction.start : latestContraction.end;
     const allDiff = dateDiffInSeconds(startDate, contractions[2].start);
-    return <>{recentDiffDisplay}<div className={allDiff < 10*60 ? 'all-diff alert' : 'all-diff'}><label>Time for last 3 contractions</label> {formatTime(allDiff)}</div></>
+    return <>{recentDiffDisplay}<div className={allDiff < 10*60 ? 'all-diff alert' : 'all-diff'}><label>Time for last 3 contractions</label> {formatDuration(allDiff)}</div></>
   } else {
     return recentDiffDisplay;
   }
 }
 
-const TimeSinceLastContraction: React.FunctionComponent<{latestContraction: IContraction}> = ({latestContraction}) => {
+const TimeSinceLastContraction: React.FunctionComponent<{latestContraction: IContraction, recentDiff: number}> = ({latestContraction, recentDiff}) => {
   const [duration, setDuration] = useState(0);
   
   useEffect(() => {
@@ -129,9 +137,13 @@ const TimeSinceLastContraction: React.FunctionComponent<{latestContraction: ICon
     return null;
   } else {
     return(
-      <div className="timer">
+      <div className={duration.valueOf() > recentDiff ? 'timer alert' : 'timer'}>
         <label>Time since last Contraction</label>
-        {formatTime(duration)}
+        <span className="timer">
+          <img className="stopwatch-icon" src={stopwatch} />
+          {formatDuration(duration)}
+        </span>
+
         </div>
     )
   }
@@ -139,16 +151,37 @@ const TimeSinceLastContraction: React.FunctionComponent<{latestContraction: ICon
 
 }
 
+function padWithLeadingZero(value: Number): String{
+return value < 10 ? `0${value.toFixed(0)}`: value.toFixed(0);
+}
 
-function formatTime(totalSeconds: number){
-  let minutes = Math.floor(totalSeconds/60);
-  let seconds = totalSeconds%60;
+function formatDuration(totalSeconds: number){
+  let date = new Date(totalSeconds);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+
+  let hoursOutput = hours > 0 ? `${padWithLeadingZero(hours)}:` : ''; 
+
+  // if(seconds === 60){
+  //   seconds = 0;
+  //   minutes ++;
+  // }
   
-  return `${minutes} Minutes, ${seconds.toFixed(0)} Seconds`;
+  return `${hoursOutput}${padWithLeadingZero(minutes)}:${padWithLeadingZero(seconds)}`;
 }
 
 function dateDiffInSeconds(startDate: Date, endDate: Date): number{
-  return (startDate.valueOf() - endDate.valueOf())/1000;
+  return startDate.valueOf() - endDate.valueOf();
+}
+
+const Time: React.FunctionComponent<{date: Date}> = ({date}) => {
+  let hours = date.getHours();
+  let twelveHour = hours > 12 ? hours - 12 : hours;
+
+  return(
+    <>{padWithLeadingZero(twelveHour)}:{padWithLeadingZero(date.getMinutes())} <span className="date-meridiem">{hours > 12 ? 'PM' : 'AM'}</span></>
+  )
 }
 
 export default App;
